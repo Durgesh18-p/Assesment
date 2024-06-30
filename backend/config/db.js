7b8685c873b358b8f3e0fc333263c1613f2ac5e5
Durgesh Suryawanshi <suryadurgesh18@gmail.com>
2024-06-30 10:24:43 +0530

    databse connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
-// backend/config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`databse connected`)
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
